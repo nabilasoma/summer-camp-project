851e183b51bfa7871f8d468e339cbbcb3af9101f
unknown <nabilasoma@gmail.com>
2023-06-10 11:10:58 +0600

    added payment page and checkout page

--- src/Payment/CheckoutForm.jsx
+++ src/Payment/CheckoutForm.jsx
@@ -1,10 +1,67 @@
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 
 const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+    const [cardError, setCardError] = useState(' ');
+
+    const handleSubmit = async(event) => {
+        event.preventDefault()
+
+        if(!stripe || !elements){
+            return
+        }
+
+        const card = elements.getElement(CardElement);
+        if(card === null){
+            return
+        }
+
+        console.log('card', card)
+
+        const {error, paymentMethod} = await stripe.createPaymentMethod({
+            type: 'card',
+            card
+        })
+
+        if(error){
+            console.log('error', error);
+            setCardError(error.message)
+        }
+        else{
+            setCardError(' ')
+            console.log('paymentMethod', paymentMethod)
+        }
+
+    }
+
     return (
-        <div>
-            <h2>this is checkout form</h2>
-        </div>
+       <>
+        <form onSubmit={handleSubmit}>
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            color: '#424770',
+                            '::placeholder': {
+                                color: '#aab7c4',
+                            },
+                        },
+                        invalid: {
+                            color: '#9e2146',
+                        },
+                    },
+                }}
+            />
+            <button className="btn btn-success mt-4" type="submit" disabled={!stripe}>
+                Pay
+            </button>
+        </form>
+       {cardError && <p className="text-red-500 ml-8">{cardError}</p>}
+       </>
     );
 };
 
--- src/Payment/Payment.jsx
+++ src/Payment/Payment.jsx
@@ -1,9 +1,17 @@
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
 
 
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
     return (
-        <div>
+        <div className="w-1/2">
             <h3>this is payment page</h3>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
+
         </div>
     );
 };
